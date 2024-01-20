27797c22b5f4c92e35e52bd17344b97d255379cf
Sunny <adamcroslow@gmail.com>
2024-01-19 18:51:00 -0600

    wip

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// holipop.art
+// Sunny Holiday 2022
+
+import Handlebars from "handlebars";
+import $ from "jquery";
+
+main()
+function main () {
+
+    console.log('testing testing one two three!')
+
+}
