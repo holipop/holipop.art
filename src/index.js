cb62abbf07bd1178ab56a76a9d4f5acba2dd5a5c
Sunny <adamcroslow@gmail.com>
2024-01-19 20:01:59 -0600

    under construction !

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 // holipop.art
 // Sunny Holiday 2022
 
-import Handlebars from "handlebars";
-import $ from "jquery";
-
 main()
 function main () {
 
-    console.log('testing testing one two three!')
+    window.addEventListener('mousedown', event => {
+        const BONK_LENGTH = 50 // ms
+        const img = document.querySelector('.bonk-wip>img')
+        img.src = "../images/construction_bunny_bonk.png"
+
+        setTimeout(() => {
+            img.src = "../images/construction_bunny.png"
+        }, BONK_LENGTH)
+    })
 
 }
