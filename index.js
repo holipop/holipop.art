b9b82693c1bd69d8ed681578d187c483d327fdd6
Sunny <adamcroslow@gmail.com>
2024-01-24 21:42:34 -0600

    wip of score counter

--- index.js
+++ index.js
@@ -4,10 +4,19 @@
 main()
 function main () {
 
-    window.addEventListener('mousedown', event => {
+    let score = 0
+    const counter = document.querySelector('[data-score]')
+
+    window.addEventListener('click', event => {
         const BONK_LENGTH = 50 // ms
         const img = document.querySelector('.bonk-wip>img')
         img.src = "../images/construction_bunny_bonk.png"
+
+        score++
+        counter.innerHTML = score + ""
+
+        const frenzyChance = (score % 50) + 1 / 250
+        console.log(Math.random() >= frenzyChance)
 
         setTimeout(() => {
             img.src = "./images/construction_bunny.png"
