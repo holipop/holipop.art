76606420bcc8693c859966249e107bdf3adf24b5
Sunny <adamcroslow@gmail.com>
2024-01-27 00:02:01 -0600

    added jQuery, Less, and Handlebars

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
 // holipop.art
 // Sunny Holiday 2022
 
-main()
-function main () {
+$(window).on('click', bonkAnimation)
 
-    let score = 0
-    const counter = document.querySelector('[data-score]')
+function bonkAnimation () {
+    const BONK_LENGTH = 50 // ms
+    const img = $('[data-bonk]')[0]
+    img.src = './images/construction_bunny_bonk.png'
 
-    window.addEventListener('click', event => {
-        const BONK_LENGTH = 50 // ms
-        const img = document.querySelector('.bonk-wip>img')
-        img.src = "../images/construction_bunny_bonk.png"
-
-        score++
-        counter.innerHTML = score + ""
-
-        const frenzyChance = (score % 50) + 1 / 250
-        console.log(Math.random() >= frenzyChance)
-
-        setTimeout(() => {
-            img.src = "./images/construction_bunny.png"
-        }, BONK_LENGTH)
-    })
-
+    setTimeout(() => {
+        img.src = './images/construction_bunny.png'
+    }, BONK_LENGTH)
 }
