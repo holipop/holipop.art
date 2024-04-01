75ddd1c58d82ab2b98ea677a3a16a7f238a43af3
Sunny <adamcroslow@gmail.com>
2024-04-01 01:53:46 -0500

    i dont fuckin know man

---
+++ scripts/bonk.js
@@ -0,0 +1,22 @@
+// shove here for now
+
+window.addEventListener('click', bonkAnimation)
+window.addEventListener('click', updateScore)
+
+function bonkAnimation () {
+    const BONK_LENGTH = 50 // ms
+    const img = document.querySelector('[data-bonk]')
+    img.src = './images/construction_bunny_bonk.png'
+
+    setTimeout(() => {
+        img.src = './images/construction_bunny.png'
+    }, BONK_LENGTH)
+}
+
+let score = 0
+function updateScore () {
+    const scoreNumber = document.querySelector('[data-score]')
+
+    score++
+    scoreNumber.innerHTML = score
+}
