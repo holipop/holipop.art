855692df1e3f14c6a72ebd001bea549492ad6c01
Sunny <adamcroslow@gmail.com>
2024-04-01 03:17:24 -0500

    I DONT KNOW WHAT IM DOIIINNGGGG

--- scripts/bonk.js
+++ scripts/bonk.js
@@ -6,10 +6,10 @@ window.addEventListener('click', updateScore)
 function bonkAnimation () {
     const BONK_LENGTH = 50 // ms
     const img = document.querySelector('[data-bonk]')
-    img.src = './images/construction_bunny_bonk.png'
+    img.src = './assets/construction_bunny_bonk.png'
 
     setTimeout(() => {
-        img.src = './images/construction_bunny.png'
+        img.src = './assets/construction_bunny.png'
     }, BONK_LENGTH)
 }
 
