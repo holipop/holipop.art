adf47bc3fdc27ec08ca03be4206fb2721f8beb02
Sunny <adamcroslow@gmail.com>
2024-04-01 21:17:47 -0500

    basic document layout

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 // holipop.art
 // Sunny Holiday 2022
 
-async function main () {
-    const response = await fetch('./templates/head.hbs')
-    const content = await response.text()
-    const template = Handlebars.compile(content)
-
-    const data = {
-        page: "Home"
+(async () => {
+    const context = {
+        items: {
+            home:    { name: "home",         href: "index.html" },
+            comms:   { name: "commissions",  href: "comms.html" },
+            gallery: { name: "gallery",      href: "gallery.html" },
+            games:   { name: "games",        href: "games.html" },
+            about:   { name: "about",        href: "about.html" },
+        },
+        selected: hbs.dataset.selected
     }
-    console.log(template(data))
-}
 
-main()
+    Handlebars.registerPartial('nav', await getTemplate('templates/nav.hbs'))
+    Handlebars.registerHelper({
+        eq: (v1, v2) => v1 === v2,
+        ne: (v1, v2) => v1 !== v2,
+        lt: (v1, v2) => v1 < v2,
+        gt: (v1, v2) => v1 > v2,
+        lte: (v1, v2) => v1 <= v2,
+        gte: (v1, v2) => v1 >= v2,
+        not: pred => !pred,
+        and () {return Array.prototype.every.call(arguments, Boolean);},
+        or () {return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);}
+    });
+
+    const template = Handlebars.compile(hbs.innerHTML)
+    $('#hbs').after(template(context))
+})()
 
 async function getTemplate(path) {
     const response = await fetch(path)
-    const content = await response.text()
-    return Handlebars.compile(content)
+    return await response.text()
+    //return Handlebars.compile(content)
 }
